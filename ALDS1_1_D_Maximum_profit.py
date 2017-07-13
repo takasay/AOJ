44ff951961e9021489f7bf7e1f8d216437718149
Seishiro Takamatsu <say@Louise.local>
2017-07-13 10:36:52 +0900

    Add ALDS1_1_D

---
+++ ALDS1_1_D_Maximum_profit.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(input())
+r = []
+[r.append(int(input())) for i in range(n)]
+
+max_diff = -sys.maxsize
+min_val = r[0]
+
+for i in range(1, n):
+    diff = r[i] - min_val
+    if diff > max_diff:
+        max_diff = diff
+    if r[i] < min_val:
+        min_val = r[i]
+
+print(max_diff)
